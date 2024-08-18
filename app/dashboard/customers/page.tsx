6d224e67fe317598880fcfb16eee2ec18f01bdac
Jumael Ahmed Khan <jumael99@gmail.com>
2024-08-18 14:35:17 +0600

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Page(props) {
+    return (
+        <div>Customers Pages</div>
+    );
+}
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Page(props) {
+    return (
+        <div>Invoices Page</div>
+    );
+}
+
+export default Page;
