99654a5e42d017895950b6ce6c036f9e9280951d
Jumael Ahmed Khan <jumael99@gmail.com>
2024-08-19 22:15:12 +0600

    chapter 7 finished

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,8 +4,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
             <div className="w-full flex-none md:w-64">
-                <SideNav />
+                <SideNav /> {/*This is side bar*/}
             </div>
+
+            {/*This is children*/}
             <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
         </div>
     );
