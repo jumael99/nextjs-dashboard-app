c6e72bd27103fef3e94a0d9c9c44c00ef951cbde
Jumael Ahmed Khan <jumael99@gmail.com>
2024-09-27 16:16:05 +0600

    following chapter 10

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Loading from '@/app/dashboard/(overview)/loading'
+
+export default function Page(props) {
+    return (
+        <div>
+            <Loading/>
+        </div>
+    );
+}
